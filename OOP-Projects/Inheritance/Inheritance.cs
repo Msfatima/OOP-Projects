e5318c97505de57dd7eb4887d0cf4d39670dd272
Fatima Hamid <eng.fatimah1@gmail.com>
2024-06-26 11:50:30 +0300

    Added files
    First commit

---
+++ OOP-Projects/Inheritance/Inheritance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Projects.Inheritance
+{
+   
+    //Inheritance It’s a mechanism that allows a class to inherit properties and behaviors from another class. 
+    //First: Defind base class (parent) 
+    public class Fruits 
+    {
+        //Second: assign propreties 
+        public string Country = "Austraila";
+        //Third: Add method 
+        public void eat() //method 
+        {
+            Console.WriteLine("Eating fruits is healthy all the time");
+        }
+    }
+    //Fourth :Defind derived class (child)
+    public class Appel : Fruits
+    {
+
+        public string Color = "red";
+    }
+    public class Orang : Fruits 
+    { 
+        public string Weight { get; set; }
+       
+    }
+
+   
+
+
+
+}
---
+++ OOP-Projects/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOP_Projects.Inheritance;
+
+Console.WriteLine("Hello, World!");
+
+
+//call the drevied class
+Appel appel = new Appel();
+//call the method 
+
+appel.eat(); // return the value mentioned in the class 
+
+
+//Print 
+Console.WriteLine(appel.Country + " " + appel.Color);
